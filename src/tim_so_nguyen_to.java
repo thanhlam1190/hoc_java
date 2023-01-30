ae81920d07738a115a6ac11875727af749c70696
thanh lam <thanhlam192002@gmail.com>
2023-01-30 08:39:14 +0700

    A

---
+++ src/tim_so_nguyen_to.java
@@ -0,0 +1,25 @@
+public class tim_so_nguyen_to {
+
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 0; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean isPrimeNumber(int n) {
+
+        if (n < 2) {
+            return false;
+        }
+
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
