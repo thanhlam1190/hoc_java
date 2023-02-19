b1625bb00a26666b40626c601af3ef85622aed45
Thanh Lảm CNTT <99947746+thanhlam1190@users.noreply.github.com>
2023-02-19 19:26:47 +0700

    Add files via upload

---
+++ src/Class_Employee/src/Entry.java
@@ -0,0 +1,8 @@
+public class Entry {
+    public static void main(String[] args) {
+        Employee nv1 = new Employee("Thu", 550000);
+        Manager gd1 = new Manager("Van", 950000, 101003001 );
+        nv1.display();
+        gd1.display();
+    }
+}
